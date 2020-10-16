fa0b8744c0421e20aa76d9fb8f1ddb2e7f254cf3
eclectic_mind <52823445+eclectic-mind@users.noreply.github.com>
2020-10-16 14:47:11 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+const pattern = bracketsConfig.flat();
+const array = str.split('');
+let res = [];
+
+for (let i = 0; i < pattern.length; i += 1) {
+  let matches = array.filter((item) => item === pattern[i]);
+  if (matches.length < 1) {
+    res.push(pattern[i]);
+  }
+}
+
+return res.length > 0 ? false : true;
 }
